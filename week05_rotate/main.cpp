1b881e0d256d2b3cf5b8beba2247f70627e5adc3
goldfish0105 <79676936+goldfish0105@users.noreply.github.com>
2022-03-28 16:43:17 +0800

    Update main.cpp

--- week05_rotate/main.cpp
+++ week05_rotate/main.cpp
@@ -3,7 +3,7 @@ float angle=0;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glPushMatrix();///³Æ¤À¯x°}
+    glPushMatrix();//å‚™ä»½çŸ©é™£
         glRotatef(angle,0,0,1);
         glColor3f(1,1,0);
         glutSolidTeapot(0.3);
