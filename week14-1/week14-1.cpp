ad037ea78761ed8affdb58e0bc3ac0c94c238c2a
goldfish0105 <vicky131831@gmail.com>
2022-05-23 16:59:16 +0800

    add week14

---
+++ week14-1/week14-1.cpp
@@ -0,0 +1,11 @@
+///Ωm≤ﬂ∂}¿…°Bºg¿…°B√ˆ¿…
+#include <stdio.h>
+int main()
+{
+    FILE * fout = fopen("file.txt","w+");
+
+    print("Hello World\n");
+    forint(fout,"Hello World\n");
+
+    fclose(fout);
+}
